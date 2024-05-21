11523550765242fa2849efa35c4002d7e4af3d12
miaotouy <3133963945@qq.com>
2024-05-21 10:16:28 +0800

    Add webapp.io deployment configuration and express server

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -1,85 +1,82 @@
-import { Context } from "@netlify/edge-functions";
+// proxy.js
+const express = require('express');
+const fetch = require('node-fetch');
 
-const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
-  const picked = new Headers();
-  for (const key of headers.keys()) {
-    if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-      const value = headers.get(key);
-      if (typeof value === "string") {
-        picked.set(key, value);
-      }
+const pickHeaders = (headers, keys) => {
+  const picked = {};
+  keys.forEach(key => {
+    if (headers[key]) {
+      picked[key] = headers[key];
     }
-  }
+  });
   return picked;
 };
 
-const CORS_HEADERS: Record<string, string> = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "*",
-  "access-control-allow-headers": "*",
+const app = express();
+
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "*",
 };
 
-export default async (request: Request, context: Context) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: CORS_HEADERS,
-    });
+app.options('*', (req, res) => {
+  res.set(CORS_HEADERS).status(204).send('');
+});
+
+app.all('*', async (req, res) => {
+  const { method, headers, body, originalUrl } = req;
+
+  if (originalUrl === '/') {
+    const blankHtml = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Google PaLM API proxy on webapp.io</title>
+      </head>
+      <body>
+        <h1>Google PaLM API proxy on webapp.io</h1>
+        <p>Tips: This project uses a reverse proxy to solve problems such as location restrictions in Google APIs.</p>
+        <p>If you have any of the following requirements, you may need the support of this project.</p>
+        <ol>
+          <li>When you see the error message "User location is not supported for the API use" when calling the Google PaLM API</li>
+          <li>You want to customize the Google PaLM API</li>
+        </ol>
+        <p>For technical discussions, please visit <a href="https://simonmy.com/posts/使用netlify反向代理google-palm-api.html">https://simonmy.com/posts/使用netlify反向代理google-palm-api.html</a></p>
+      </body>
+      </html>
+    `;
+    return res.set({ ...CORS_HEADERS, "Content-Type": "text/html" }).send(blankHtml);
   }
 
-  const { pathname, searchParams } = new URL(request.url);
-  if(pathname === "/") {
-    let blank_html = `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Google PaLM API proxy on Netlify Edge</title>
-</head>
-<body>
-  <h1 id="google-palm-api-proxy-on-netlify-edge">Google PaLM API proxy on Netlify Edge</h1>
-  <p>Tips: This project uses a reverse proxy to solve problems such as location restrictions in Google APIs. </p>
-  <p>If you have any of the following requirements, you may need the support of this project.</p>
-  <ol>
-  <li>When you see the error message &quot;User location is not supported for the API use&quot; when calling the Google PaLM API</li>
-  <li>You want to customize the Google PaLM API</li>
-  </ol>
-  <p>For technical discussions, please visit <a href="https://simonmy.com/posts/使用netlify反向代理google-palm-api.html">https://simonmy.com/posts/使用netlify反向代理google-palm-api.html</a></p>
-</body>
-</html>
-    `
-    return new Response(blank_html, {
-      headers: {
-        ...CORS_HEADERS,
-        "content-type": "text/html"
-      },
+  const url = new URL(originalUrl, 'https://generativelanguage.googleapis.com');
+  const proxyHeaders = pickHeaders(headers, ['content-type', 'x-goog-api-client', 'x-goog-api-key', 'accept-encoding']);
+
+  try {
+    const proxyRes = await fetch(url, {
+      body,
+      method,
+      headers: proxyHeaders,
     });
+
+    const proxyHeaders = {
+      ...CORS_HEADERS,
+      ...Object.fromEntries(proxyRes.headers.entries()),
+    };
+
+    delete proxyHeaders['content-encoding'];
+
+    proxyRes.body.pipe(res.set(proxyHeaders).status(proxyRes.status));
+  } catch (err) {
+    res.status(500).send(err.message);
   }
+});
 
-  const url = new URL(pathname, "https://generativelanguage.googleapis.com");
-  searchParams.delete("_path");
-
-  searchParams.forEach((value, key) => {
-    url.searchParams.append(key, value);
-  });
-
-  const headers = pickHeaders(request.headers, ["content-type", "x-goog-api-client", "x-goog-api-key", "accept-encoding"]);
-
-  const response = await fetch(url, {
-    body: request.body,
-    method: request.method,
-    duplex: 'half',
-    headers,
-  });
-
-  const responseHeaders = {
-    ...CORS_HEADERS,
-    ...Object.fromEntries(response.headers),
-    "content-encoding": null
-  };
-
-  return new Response(response.body, {
-    headers: responseHeaders,
-    status: response.status
-  });
-};
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
